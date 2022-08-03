1c23a0cca3f96bda4306e04928f9b1f7664dcbfe
Ilya Eventov <bnwombis@gmail.com>
2022-08-03 20:58:55 +0300

    refactor main page

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/graph', [App\Http\Controllers\GraphController::class, 'index'])->name('graph_index');
