45ce1519afe2d32bcb210b1fb28c75dafb83106a
Ilya Eventov <bnwombis@gmail.com>
2022-08-03 17:41:35 +0300

    add lorem to graphs main page

--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -4,18 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus ad, aliquid cumque delectus ea eligendi enim excepturi exercitationem ipsum laborum laudantium, libero natus perspiciatis praesentium qui quod recusandae unde.
             </div>
         </div>
     </div>
