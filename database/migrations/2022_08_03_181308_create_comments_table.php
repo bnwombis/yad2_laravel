b24e4260dc6b11d6b577e29598309cd5492563b7
Ilya Eventov <bnwombis@gmail.com>
2022-08-03 21:21:21 +0300

    add comments migration

---
+++ database/migrations/2022_08_03_181308_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string("username");
+            $table->string("email");
+            $table->string("message");
+            $table->integer("city_id");
+            $table->timestamps();
+            $table->foreign("city_id")->references("id")->on("cities_ads_stats")
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
